c0bca194c388bfb2dfb5aaeed54a4b74385a7384
busungg <busungg@naver.com>
2022-01-02 21:14:24 +0900

    Updated 블록체인 프로젝트
    1. 블록체인 튜토리얼 프로젝트 업데이트

--- js/eth-todo-list/contracts/TodoList.sol
+++ js/eth-todo-list/contracts/TodoList.sol
@@ -1,6 +1,27 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-    uint256 public taskCount = 1000;
-    uint256 public taskCount2 = 5;
+    uint public taskCount = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    constructor() public {
+        createTask("Check out dappuniversity.com");
+        createTask("1");
+        createTask("2");
+        createTask("3");
+        createTask("4");
+        createTask("5");
+    }
+
+    mapping(uint => Task) public tasks;
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
